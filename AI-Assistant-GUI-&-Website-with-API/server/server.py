0a45ec226581bc5bb92804dab7758d3420ab19b5
HemangGour <hemanggour547@gmail.com>
2024-11-30 18:49:51 +0530

    Added web support

--- AI-Assistant-GUI-&-Website-with-API/server/server.py
+++ AI-Assistant-GUI-&-Website-with-API/server/server.py
@@ -53,6 +53,25 @@ class HandleUser(Resource):
 
 api.add_resource(HandleUser, "/api")
 
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        message = request.form.get('message')
+        if message:
+            content = {
+                'parts': [
+                    {'text': 'Previous chats:\n' + '\n'.join(userChats)},
+                    {'text': 'Current message: ' + message}
+                ]
+            }
+            model = AI.GenerateContent(content)
+            if len(userChats) >= 10:
+                userChats.pop(0)
+                modelChats.pop(0)
+            userChats.append(message)
+            modelChats.append(model)
+            return render_template('chat.html', range=range(0, len(userChats)), modelChats=modelChats, userChats=userChats)
+    return render_template('chat.html')
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
